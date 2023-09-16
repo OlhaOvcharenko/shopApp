18fe619db9e481b46261c8013391703a11775675
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-09-16 17:10:27 +0300

    change component product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,43 +2,82 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-
+import { useState } from 'react';
 
 const Product = props => {
+
+  const initialSize = props.sizes[0].name;
+
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(initialSize);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const handleClickColor = (color) => {
+    setCurrentColor(color);
+  };
+
+  const handleClickSize = (size) => {
+    setCurrentSize(size);
+  };
+
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>{props.basePrice}$</span>
         </header>
         <form>
+
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
+            
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map((size) => (
+                <li key={size.name}>
+                  <button type="button" className={clsx(styles.sizeButton, {
+                      [styles.active]: size.name === currentSize,
+                    })}
+                    onClick={() => handleClickSize(size.name)}
+                    >
+                    {size.name}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
+
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map((color) => (
+                <li key={color}>
+                  <button
+                    type="button"
+                    className={clsx(prepareColorClassName(color), {
+                      [styles.active]: color === currentColor,
+                    })}
+                    onClick={() => handleClickColor(color)}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
+
           <Button className={styles.button}>
             <span className="fa fa-shopping-cart" />
           </Button>
+
         </form>
       </div>
     </article>
@@ -46,8 +85,16 @@ const Product = props => {
 };
 
 Product.propTypes = {
-  name: PropTypes.string,
-  
-}
+  name: PropTypes.string.isRequired, // Required string
+  title: PropTypes.string.isRequired, // Required string
+  basePrice: PropTypes.number.isRequired, // Required number
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired, // Required array of strings
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired, // Required string
+      additionalPrice: PropTypes.number.isRequired, // Required number
+    })
+  ).isRequired, // Required array of objects with specific shape
+};
 
 export default Product;
